b5621819559a4ee9f37e0bd372622e06086e36c0
skaduhs5232 <thiagooliveira1039@gmail.com>
2024-08-27 00:53:52 -0300

    loader

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,13 +1,22 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxhD6da0dvx3rQgGChA5NsfQ7hLE7wGTYIfTSfMEoa-esFP9K2vpPUinj6v_xhkXB2sTg/exec'
-
-
 const form = document.forms['contact-form']
-
+const loading = document.getElementById('loading')
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  
+  loading.style.display = 'flex'
+  
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+    .then(response => {
+      alert("Obrigado, seu cadastro foi adicionado, fique de olho na data!")
+    })
+    .then(() => {
+      loading.style.display = 'none'
+      window.location.reload()
+    })
+    .catch(error => {
+      console.error('Error!', error.message)
+      loading.style.display = 'none' 
+    })
 })
