cc14092cb7d2600097b3425807bbf408e807b2e6
Thiago Sampaio <thiagooliveira1039@gmail.com>
2024-08-27 15:13:10 -0300

    melhoria index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+  },
+});
