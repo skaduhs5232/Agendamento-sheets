3428025ed2c11f21f38b6839e5f54398eeb8fde7
Thiago de Oliveira Sampaio <104845945+skaduhs5232@users.noreply.github.com>
2024-09-04 13:04:41 -0300

    Update Google Sheet.js

--- agendamento/Google Sheet.js
+++ agendamento/Google Sheet.js
@@ -1,4 +1,14 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbzEDOu7dFI2mE79PeniKjgyoQjx0A9l7iNU5CdNjf6HC1yvcCo7XKVFlKISnB89C2ntTQ/exec";
+
+
+// Este link é para a planilha "mestre" (administradora).
+var mainURL = "https://script.google.com/macros/s/AKfycbzEDOu7dFI2mE79PeniKjgyoQjx0A9l7iNU5CdNjf6HC1yvcCo7XKVFlKISnB89C2ntTQ/exec";
+
+// Este link é para a planilha individual de cada psicólogo.
+var scriptURL = "";
+
+// Este é um dicionário que contém todos os nomes e links para cada planilha.
+let planilhasPsicologos = {};
+
 const form = document.forms["contact-form"];
 const loading = document.getElementById("loading");
 
@@ -52,54 +62,94 @@ form.addEventListener("submit", async (e) => {
   formData.set("Nome", nome);
   formData.set("Psicólogo", psicologo);
 
-  fetch(scriptURL, {
+  fetch(mainURL, {
     method: "POST",
     body: formData,
   })
-    .then((response) => response.json())
-    .then((result) => {
-      if (result.result === "success") {
-        showFeedback("Obrigado, seu cadastro foi adicionado, fique de olho na data!", "success");
-      } else if (result.result === "error" && result.message === "agendamento duplicado") {
-        showFeedback("Este agendamento já foi feito. Por favor, selecione outro horário.", "warning");
-      } else {
-        showFeedback("Erro: " + result.message, "error");
-      }
-      form.reset();
-    })
-    .catch((error) => {
-      console.error("Error!", error.message);
-      showFeedback("Houve um erro ao enviar os dados. Tente novamente mais tarde.", "error");
-    })
-    .finally(() => {
-      loading.style.display = "none";
-    });
+  .then((response) => response.json())
+  .then((result) => {
+    
+    if (result.result === "success") {
+      
+      showFeedback("Obrigado, seu cadastro foi adicionado, fique de olho na data!", "success");
+
+      // Agora, envie a mesma informação para a planilha individual.
+      // TODO: FIX! Consertar essa parte
+      fetch(scriptURL, { method: "POST", body: formData}).then((res) => res.json())
+
+    } else if (result.result === "error" && result.message === "agendamento duplicado") {
+      
+      showFeedback("Este agendamento já foi feito. Por favor, selecione outro horário.", "warning");
+
+    } else {
+
+      showFeedback("Erro: " + result.message, "error");
+
+    }
+
+    form.reset();
+  })
+  .catch((error) => {
+    console.error("Error!", error.message);
+    showFeedback("Houve um erro ao enviar os dados. Tente novamente mais tarde.", "error");
+  })
+  .finally(() => {
+    loading.style.display = "none";
+  });
+
+
+
 });
 
 // Inicializa outras funcionalidades da página após o DOM carregar
 document.addEventListener("DOMContentLoaded", function () {
+  
   const menuToggle = document.querySelector(".menu-toggle");
   const navMenu = document.querySelector("nav ul");
+  const selectProfissional = document.getElementById("profissional")
+
+  selectProfissional.setAttribute("disabled", "")
 
   menuToggle.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-  });
+    navMenu.classList.toggle("active")
+  })
+
+  selectProfissional.addEventListener("change", event => {
+
+    let profissionalSelecionado = planilhasPsicologos.filter(x => x.nome == event.target.value)[0]
+    console.log(`Profissional ${profissionalSelecionado.nome} selecionado!`)
+
+    scriptURL = profissionalSelecionado.url
+
+  })
 
   const dataInput = form.querySelector('input[name="Data"]');
   const dataAtual = new Date().toISOString().split("T")[0];
+
   dataInput.setAttribute("min", dataAtual);
 
   // Carregar os psicólogos da planilha
   fetch("https://script.google.com/macros/s/AKfycbwDM7wIH0c3FOzgw6Y-9MxVI7alohX_sw2MIg62OfVGcCdA9k_rvsojZefj7YD3Z_jB/exec")
+    
     .then(response => response.json())
     .then(data => {
+      
       const selectPsicologo = document.querySelector('select[name="Psicólogo"]');
-      data.forEach(optionText => {
+      planilhasPsicologos = data
+
+      data.forEach(x => {
+        
         const option = document.createElement("option");
-        option.value = optionText;
-        option.textContent = optionText;
-        selectPsicologo.appendChild(option);
-      });
+        
+        option.value = x["nome"]
+        option.textContent = x["nome"]
+        selectPsicologo.appendChild(option)
+
+      })
+
+      selectProfissional.removeAttribute("disabled")
+
     })
     .catch(error => console.error("Erro ao carregar os dados:", error));
+
 });
