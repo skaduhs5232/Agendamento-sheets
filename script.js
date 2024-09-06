767d40afe655b26ed46bd6af235e24186e856a31
skaduhs5232 <thiagooliveira1039@gmail.com>
2024-09-05 22:08:08 -0300

    começo de escolha da paleta de cores

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ document.addEventListener("DOMContentLoaded", function() {
         menu.classList.toggle('active');
     });
 });
+
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar'); // Seleciona a navbar
+
+// Função para monitorar o scroll
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Se rolou para baixo, esconde a navbar, se rolou para cima, mostra
+    if (scrollTop > lastScrollTop) {
+        navbar.classList.add('navbar-hidden'); // Esconde ao rolar para baixo
+    } else {
+        navbar.classList.remove('navbar-hidden'); // Mostra ao rolar para cima
+    }
+
+    // Atualiza a última posição de rolagem
+    lastScrollTop = scrollTop;
+});
