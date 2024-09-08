dc8571ee4c01b2fefd2b745427b37f8408b4e879
skaduhs5232 <thiagooliveira1039@gmail.com>
2024-09-08 13:38:11 -0300

    swiper

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var swiper = new Swiper('.swiper-container', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-    });
-
-    document.getElementById('menu-toggle').addEventListener('click', function() {
-        var menu = document.getElementById('menu');
-        menu.classList.toggle('active');
-    });
-});
 
 let lastScrollTop = 0;
 const navbar = document.querySelector('.navbar'); // Seleciona a navbar
